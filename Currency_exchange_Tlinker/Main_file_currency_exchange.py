7d7381a5843402b5830f2236004b9c1c412e14b4
Valentin Gardev <bestmaad@gmail.com>
2025-06-02 00:17:45 +0300

    Making skeleton of main functions

--- Currency_exchange_Tlinker/Main_file_currency_exchange.py
+++ Currency_exchange_Tlinker/Main_file_currency_exchange.py
@@ -10,5 +10,25 @@ def show_live_currency_values():
     pass
 
 
+def check_if_int():
+    """
+    Checks if needed input is an integer, if not, prints appropriate response
+    """
+    pass
+
+
 def menu():
+    """
+    Menu inputs for selecting currencies:
+    """
+    option_input = input()
+
+    if option_input == 1:
+        pass
+    elif option_input == 2:
+        pass
+    elif option_input == 3:
+        pass
+    elif option_input == 4:
+        pass
     pass
