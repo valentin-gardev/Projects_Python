7049cf991604a59410ceb8b466dc9537c6faef3a
Valentin Gardev <bestmaad@gmail.com>
2025-06-15 01:21:11 +0300

    Update Tlinker_file.py

--- Currency_exchange_Tlinker/Tlinker_file.py
+++ Currency_exchange_Tlinker/Tlinker_file.py
@@ -1,12 +1,25 @@
-import requests
+# import requests
+#
+# def convert_currency(amount, base_currency, target_currency):
+#     url = f"https://moneymorph.dev/api/convert/{amount}/{base_currency}/{target_currency}"
+#     response = requests.get(url)
+#     data = response.json()
+#
+#     result = data['response']
+#     print(f"{amount} {base_currency} = {result:.2f} {target_currency}")
+#
+#
+# # Example
+# convert_currency(109, 'JPY', 'BGN')
 
-def convert_currency(amount, base_currency, target_currency):
-    url = f"https://api.exchangerate.host/convert?from={base_currency}&to={target_currency}&amount={amount}"
-    response = requests.get(url)
-    data = response.json()
-    result = data['result']
-    print(f"{amount} {base_currency} = {result:.2f} {target_currency}")
+from tkinter import *
 
-# Example
-convert_currency(100, 'INR', 'JPY')
-convert_currency(250, 'GBP', 'CAD')
+window = Tk() # instantiate an instance of a window
+window.geometry("420x420") # changes size of window
+window.title("Currency exchange program")
+
+icon = PhotoImage(file='currency_exchange_logo.png')  # Converts image to 'PhotoImage' that the tkinter can use
+window.iconphoto(True, icon) # Function that can use the photoimage
+window.config(background='black') # changes background colour, can use hex value
+
+window.mainloop()  # places window on screen and listens to events
