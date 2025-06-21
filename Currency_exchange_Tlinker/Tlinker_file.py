84432808fd70aca16214f8cff9437421d5b688e7
Valentin Gardev <bestmaad@gmail.com>
2025-06-22 01:58:35 +0300

    Update Tlinker_file.py

--- Currency_exchange_Tlinker/Tlinker_file.py
+++ Currency_exchange_Tlinker/Tlinker_file.py
@@ -18,24 +18,24 @@ window = Tk()  # instantiate an instance of a window
 window.geometry("420x420")  # changes size of window
 window.title("Currency exchange program")
 
-icon = PhotoImage(file='currency_exchange_logo.png')  # Converts image to 'PhotoImage' that the tkinter can use
-window.iconphoto(True, icon)  # Function that can use the photoimage
-window.config(background='black')  # changes background colour, can use hex value
+# icon = PhotoImage(file='currency_exchange_logo.png')  # Converts image to 'PhotoImage' that the tkinter can use
+# window.iconphoto(True, icon)  # Function that can use the photoimage
+# window.config(background='black')  # changes background colour, can use hex value
 
-label = Label(window,
-              text='Hello world maybesss?',
-              font=('Arial',40,'bold'),
-              fg='green',
-              bg='red',
-              relief=RAISED, # Setting type of border
-              bd=10,    # Border width
-              padx=20,  # Space right and left of border
-              pady=20,  # Space top and bottom of border
-              image=icon,  # Placing image into the label
-              compound='bottom')
-# Creates a label, an area widget that hold text or image within a window
-#  Default label, lebel.pack()
-label.place(x=0,y=0)
+# label = Label(window,
+#               text='Hello world maybesss?',
+#               font=('Arial',40,'bold'),
+#               fg='green',
+#               bg='red',
+#               relief=RAISED, # Setting type of border
+#               bd=10,    # Border width
+#               padx=20,  # Space right and left of border
+#               pady=20,  # Space top and bottom of border
+#               image=icon,  # Placing image into the label
+#               compound='bottom')
+# # Creates a label, an area widget that hold text or image within a window
+# #  Default label, lebel.pack()
+# label.place(x=0,y=0)
 def click():
     print("Button clicked")
 button = Button(window,
@@ -47,5 +47,17 @@ button = Button(window,
                 activebackground='Black',
                 activeforeground='White')
 button.pack()
+
+
+#Radiobuttons
+
+choice_currency = ['EUR', 'AUD', 'BGN', 'JYP']
+for index in range(len(choice_currency)):
+    radio_button_currency_checker = Radiobutton(window, text=choice_currency[index])
+
+    radio_button_currency_checker.pack()
+
+
+
 
 window.mainloop()  # places window on screen and listens to events
