88b5e397010b5c25b3476014bb3e70d27a021b00
Valentin Gardev <bestmaad@gmail.com>
2025-07-08 21:39:09 +0300

    Update main_page.py

--- Phonebook_Personal_User/main_page.py
+++ Phonebook_Personal_User/main_page.py
@@ -1,4 +1,15 @@
 from tkinter import *
+# Functions of the different options
+
+
+def sign_in_button():
+    pass
+
+
+def sign_up_button():
+    pass
+
+
 #  Main menu
 main_menu_window = Tk()
 main_menu_window.geometry('420x420')
@@ -16,7 +27,16 @@ menu_label = Label(main_menu_window,
                    bg='#6A2ED9'
                    )
 
+# Sign in button
+button_menu_sign_in = Button(main_menu_window,
+                             text='Sign in')
+
+# Sign up button
+button_menu_sign_up = Button(main_menu_window,
+                             text='Sign up')
+
 # GUI packs
 menu_label.pack()
+button_menu_sign_in.pack(pady=5)
+button_menu_sign_up.pack()
 main_menu_window.mainloop()
-
