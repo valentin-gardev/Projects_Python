e6ec269f0cf969523860ccca9323a066285827e8
Valentin Gardev <bestmaad@gmail.com>
2025-07-18 02:17:21 +0300

    Update Main_file_currency_exchange.py

--- Currency_exchange_Tlinker/Main_file_currency_exchange.py
+++ Currency_exchange_Tlinker/Main_file_currency_exchange.py
@@ -1,117 +1,120 @@
 from tkinter import *
-import requests
+# Functions of the different options
 
 
-def currency_conversion(amount: int, base_currency: str, target_currency: str):
-    """
-     _ Website used for free currency exchange ratios https://moneymorph.dev
-     - URL showing all the available currencies https://moneymorph.dev/api/currencies
-    """
-    url = f"https://moneymorph.dev/api/convert/{amount}/{base_currency}/{target_currency}"
-    response = requests.get(url)
-    data = response.json()
-    result = data['response']
-    return result
+def select_frame(frame):
+    """Hide all frames and show only the selected one"""
+    for f in (login_frame, sign_up_frame):
+        f.pack_forget()
+    frame.pack(fill='both', expand=True)
 
 
-def clicked_conversion_button():
-    """
-    Conversion button is clicked:
-    - gets the two radio button selections(from/to)
-    - calls function checking if the Amount text given is a string
-    - calls currency_conversion_api function that converts the given amount from x to y currency
-    - updates the converted currency label
-    """
-    selected_from = select_from.get()
-    selected_to = select_to.get()
-    amount_put_for_conversion = check_if_string()
-    # Making everything a string, so it can easily be put together in the final output
-    converted_amount = str(currency_conversion(amount_put_for_conversion, selected_from, selected_to))
-    amount_put_for_conversion = str(amount_put_for_conversion)
-    label_converted_currency.config(text=amount_put_for_conversion + ' ' + selected_from + ' = ' + converted_amount + ' ' + selected_to)
+
+#  App Window
+app_window = Tk()
+app_window.geometry('420x420')
+app_window.title('Main Menu')
+app_window.config()
+
+# ___ Frames ___
+login_frame = Frame(app_window, background='#9431ce')
+sign_up_frame = Frame(app_window, background='#9431ce')
+account_frame = Frame(app_window, background='#9431ce')
+
+# ___ Login Frame ___
+login_title_label = Label(login_frame,
+                    text='Log in',
+                    bd=13,
+                    fg='#D9D02E',
+                    font=('Ariel', 20, "bold"),
+                    relief=RAISED,
+                    padx=10,
+                    pady=10,
+                    bg='#6A2ED9'
+                    )
+login_username_label = Label(login_frame,
+                       text='Username',
+                       background='#9431ce')
+login_username_entry = Entry(login_frame,
+                       )
+login_password_label = Label(login_frame,
+                       text='Password',
+                       background='#9431ce')
+login_password_entry = Entry(login_frame,
+                       )
+login_button_menu_sign_in = Button(login_frame,
+                             text='Sign in')
+
+login_button_menu_sign_up = Button(login_frame,
+                             text='Sign up',
+                             command=lambda:select_frame(sign_up_frame)) # runs first function with no argument, when clicked runs function with argument
+# ___ Sign up Frame ___
+title_label = Label(sign_up_frame,
+                    text='Create an Account',
+                    bd=13,
+                    fg='#D9D02E',
+                    font=('Ariel', 20, "bold"),
+                    relief=RAISED,
+                    padx=10,
+                    pady=10,
+                    bg='#6A2ED9'
+                    )
+username_label = Label(sign_up_frame,
+                       text='Username',
+                       background='#9431ce')
+username_entry = Entry(sign_up_frame,
+                       )
+password_label = Label(sign_up_frame,
+                       text='Password',
+                       background='#9431ce')
+password_entry = Entry(sign_up_frame,
+                       )
+button_menu_sign_in = Button(sign_up_frame,
+                             text='Sign in')
+
+button_menu_sign_up = Button(sign_up_frame,
+                             text='Sign up')
+# ___ Account frame ___
+account_title_label = Label(account_frame,
+                    text='Welcome (name of account)',
+                    bd=13,
+                    fg='#D9D02E',
+                    font=('Ariel', 20, "bold"),
+                    relief=RAISED,
+                    padx=10,
+                    pady=10,
+                    bg='#6A2ED9'
+                    )
+button_browse_contacts_ac = Button(account_frame,
+                             text='Browse Contacts')
+button_change_password_ac = Button(account_frame,
+                             text='Change Password')
+button_delete_account_ac = Button(account_frame,
+                             text='Delete Account')
+# ___ Login frame pack ___
+
+login_title_label.pack()
+login_username_label.pack()
+login_username_entry.pack()
+login_password_label.pack()
+login_password_entry.pack()
+login_button_menu_sign_in.pack(pady=5)
+login_button_menu_sign_up.pack()
+
+# ___ Sign up frame pack ___
+title_label.pack()
+username_label.pack()
+username_entry.pack()
+password_label.pack()
+password_entry.pack()
+button_menu_sign_up.pack(pady=5)
+button_menu_sign_in.pack()
+# ___ Account frame pack ___
+account_title_label.pack()
+button_delete_account_ac.pack()
+button_change_password_ac.pack()
+button_browse_contacts_ac.pack()
 
 
-def check_if_string():  # Check if the input amount text is an integer
-    amount_input = text_amount_input.get('1.0', END)
-    try:
-        amount_input = int(amount_input)
-        return amount_input
-    except:
-        label_converted_currency.config(text='Please input a natural number')
-# def test_print()
-
-currency_exchange_window = Tk()
-currency_exchange_window.geometry('420x320')
-currency_exchange_window.title("Currency exchange program")
-window_icon = PhotoImage(file='currency_exchange_logo.png')  # Converts image to 'PhotoImage' that the tkinter can use
-currency_exchange_window.iconphoto(True, window_icon)  # Function that can use the photoimage
-# LABEL title
-label_program = Label(currency_exchange_window,
-                      text='Currency Converter',
-                      bd=13,
-                      fg='red',
-                      font=('Ariel', 25, "bold"),
-                      relief=RAISED,
-                      padx=10,
-                      pady=10
-                      )
-# LABEL from / to currencies
-label_currency_from = Label(currency_exchange_window,
-                            text='From:')
-label_currency_to = Label(currency_exchange_window,
-                          text='To:')
-# RADIO BUTTONS options to convert FROM
-available_currencies = ['EUR', 'AUD', 'BGN', 'JPY']
-select_from = StringVar()
-frame_currency_buttons_from = Frame(currency_exchange_window)  # Creating a frame of the currency radio buttons from
-for currency in available_currencies:
-    radio_button_currencies_from = Radiobutton(frame_currency_buttons_from,
-                                               text=currency,
-                                               variable=select_from,
-                                               value=currency,
-                                               )
-    radio_button_currencies_from.pack(side='left')
-
-select_from.set('EUR')  # Setting the starting selected button with the index
-# RADIO BUTTONS options to convert TO
-available_currencies = ['EUR', 'AUD', 'BGN', 'JPY']
-select_to = StringVar()
-
-frame_currency_buttons_to = Frame(currency_exchange_window)  # Creating a frame of the currency radio buttons to
-for currency in available_currencies:
-    radio_button_currencies_to = Radiobutton(frame_currency_buttons_to,
-                                             text=currency,
-                                             variable=select_to,
-                                             value=currency,
-                                             )
-    radio_button_currencies_to.pack(side='left')
-select_to.set('EUR')
-
-# LABEL and TEXT for amount
-currency_amount = Label(currency_exchange_window,
-                        text='Amount:')
-text_amount_input = Text(currency_exchange_window,
-                         height=1,
-                         width=10,
-                         )
-
-# Conversion BUTTON
-# Check if the Amount label is a string or float, if it is string, print message, output a message box if it is not int
-
-conversion_button = Button(currency_exchange_window,
-                           text='Convert',
-                           command=clicked_conversion_button,
-                           )
-
-label_converted_currency = Label(currency_exchange_window,
-                                 text='')
-label_program.pack()
-label_currency_from.pack()
-frame_currency_buttons_from.pack()
-label_currency_to.pack()
-frame_currency_buttons_to.pack()
-currency_amount.pack()
-text_amount_input.pack()
-conversion_button.pack()
-label_converted_currency.pack()
-currency_exchange_window.mainloop()
+select_frame(login_frame)
+app_window.mainloop()
