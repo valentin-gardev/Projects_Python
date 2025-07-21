c7be9a3820923e7116d4af6d64f49d63e8a52ea9
Valentin Gardev <bestmaad@gmail.com>
2025-07-21 19:23:55 +0300

    Update test_database.py

--- Currency_exchange_Tlinker/test_database.py
+++ Currency_exchange_Tlinker/test_database.py
@@ -11,6 +11,11 @@ cursor = conn.cursor()
 # Create a database 'accounts' where accounts would be stored with id, username, password
 cursor.execute('CREATE DATABASE IF NOT EXISTS accounts')
 
-cursor.execute('CREATE TABLE IF NOT EXISTS id INT PRIMARY KEY AUTOINCREMENT,'
-               'username TEXT UNIQUE NOT NULL,'
-               'password TEXT NOT NULL')
+cursor.execute('CREATE TABLE IF NOT EXISTS person (id INT PRIMARY KEY, name VARCHAR(64))')
+cursor.execute('''
+INSERT INTO person (id,name) VALUES (1, 'greqg'); ''')
+conn.commit()
+cursor.execute('SELECT * FROM person')
+result = cursor.fetchall()
+for table in result:
+    print(table)
