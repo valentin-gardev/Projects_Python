0fdc3c05dd323d98efb9690e5097781ab7c76116
Valentin Gardev <bestmaad@gmail.com>
2025-07-21 15:53:30 +0300

    Update test_database.py

--- Currency_exchange_Tlinker/test_database.py
+++ Currency_exchange_Tlinker/test_database.py
@@ -3,7 +3,14 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='123456'
+    password='123456',
+    database='accounts'
 )
 
-print(conn)
+cursor = conn.cursor()
+# Create a database 'accounts' where accounts would be stored with id, username, password
+cursor.execute('CREATE DATABASE IF NOT EXISTS accounts')
+
+cursor.execute('CREATE TABLE IF NOT EXISTS id INT PRIMARY KEY AUTOINCREMENT,'
+               'username TEXT UNIQUE NOT NULL,'
+               'password TEXT NOT NULL')
