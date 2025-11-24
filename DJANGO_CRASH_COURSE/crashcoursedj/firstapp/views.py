d1af7ad579790cd3eecf5b8ae82528a80fb6914c
Valentin Gardev <bestmaad@gmail.com>
2025-11-25 01:09:05 +0200

    Adding app
    adding app into INSTALLED_APPS in SETINGS

--- DJANGO_CRASH_COURSE/crashcoursedj/firstapp/views.py
+++ DJANGO_CRASH_COURSE/crashcoursedj/firstapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+def hello_world(request):
+    return HttpResponse('Hello world')
+
+class HelloEthiopia(View):
+    def get(self, request):
+        return HttpResponse('HelloEthiopia')  
