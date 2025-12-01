f5f14b747db91489ff93a3a6422f736b988b7b0d
Valentin Gardev <bestmaad@gmail.com>
2025-12-01 16:48:18 +0200

    HTML
    Creating an HTML template and linking it with views, urls and the form. Afterwards testing it works. Got errors it didn't, I forgot to save the files.

--- DJANGO_CRASH_COURSE/crashcoursedj/firstapp/urls.py
+++ DJANGO_CRASH_COURSE/crashcoursedj/firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('function', views.hello_world),
     path('class', views.HelloEthiopia.as_view()),
+    path('reservation', views.home),
 ]
--- DJANGO_CRASH_COURSE/crashcoursedj/firstapp/views.py
+++ DJANGO_CRASH_COURSE/crashcoursedj/firstapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 # Create your views here.
 def hello_world(request):
     return HttpResponse('Hello world')
 
 class HelloEthiopia(View):
     def get(self, request):
-        return HttpResponse('HelloEthiopia')  
+        return HttpResponse('HelloEthiopia')
+    
+
+def home(request):
+    form = ReservationForm
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Success')
+
+    return render(request, 'index.html', {'form' : form })
