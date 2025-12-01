1369dea9c5617faaf1a21a04c9e46d85b51285f0
Valentin Gardev <bestmaad@gmail.com>
2025-12-01 16:19:19 +0200

    adding tables
    Making a new model and then form from that model, also creating a superuser So I can login into the django admin panel

--- DJANGO_CRASH_COURSE/crashcoursedj/firstapp/models.py
+++ DJANGO_CRASH_COURSE/crashcoursedj/firstapp/models.py
@@ -6,3 +6,9 @@ class MenuItem(models.Model):
     surname = models.CharField(max_length=255)
     price = models.IntegerField()
 
+class Reservation(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    guest_count = models.IntegerField()
+    reservation_time = models.DateField(auto_now=True)
+    comments = models.CharField(max_length=1000)
